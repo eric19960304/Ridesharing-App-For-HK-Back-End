f950545588856db8812e63db0419871a7afac568
Eric Lau <ericlauchiho@gmail.com>
2018-11-07 22:39:11 +0800

    add production mode

--- app.js
+++ app.js
@@ -2,22 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const privateKey  = fs.readFileSync('/ssl_cert/server.key', 'utf8');
-const certificate = fs.readFileSync('/ssl_cert/server.crt', 'utf8');
-const ca = fs.readFileSync('/ssl_cert/ca.crt', 'utf8');
 
 const userRouter = require('./src/controllers');
 const config = require('./config');
 
 const app = express();
-const credentials = {
-    key: privateKey, 
-    cert: certificate,
-    ca
-};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -45,11 +36,24 @@ All routes:
 console.log('using config:', config);
 
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
     console.log('Server is running on Port 80');
 });
-httpsServer.listen(443, () => {
-    console.log('Server is running on Port 443');
-});
+
+if(process.env.PROD){
+    console.log('production mode');
+    const fs = require('fs');
+    const privateKey  = fs.readFileSync('/ssl_cert/server.key', 'utf8');
+    const certificate = fs.readFileSync('/ssl_cert/server.crt', 'utf8');
+    const ca = fs.readFileSync('/ssl_cert/ca.crt', 'utf8');
+    const credentials = {
+        key: privateKey, 
+        cert: certificate,
+        ca
+    };
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(443, () => {
+        console.log('Server is running on Port 443');
+    });
+}
