425da630c2f5ec77b14759467e51f7e8cffabd74
Eric Lau <ericlauchiho@gmail.com>
2018-11-26 04:41:14 +0800

    fix prod mongodb connection

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
-
+let mongodb_url = 'mongodb://localhost:27017/threeriders';
+const db_username = 'root';
+const db_password = process.env.MONGODB_PASSWORD;
+if(process.env.PROD){
+    mongodb_url = `mongodb://${db_username}:${db_password}@localhost:27017/threeriders`;
+}
 const config = {
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/threeriders',
+    MONGODB_URI: mongodb_url,
     jwt_secret: process.env.JWT_SECRET || 'threeriders',
     mailConfig: {
         user: 'hkucsfyp2018threeriders@gmail.com',
