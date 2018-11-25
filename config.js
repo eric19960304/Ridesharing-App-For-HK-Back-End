40053887e0ddf064d91a4fd0746c08108c031618
Eric Lau <ericlauchiho@gmail.com>
2018-11-26 05:14:11 +0800

    fix mongoose...

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let mongodb_url = 'mongodb://localhost:27017/threeriders';
 const db_username = 'root';
 const db_password = process.env.MONGODB_PASSWORD;
 if(process.env.PROD){
-    mongodb_url = `mongodb://${db_username}:${db_password}@localhost:27017/threeriders`;
+    mongodb_url = `mongodb://${db_username}:${db_password}@localhost:27017/threeriders?authSource=admin`;
 }
 const config = {
     MONGODB_URI: mongodb_url,
