471bd7ecd230daa43f5500b117a08d8e8cc791ec
Eric Lau <ericlauchiho@gmail.com>
2018-12-02 05:06:17 +0800

    fix user models

--- src/models/user/index.js
+++ src/models/user/index.js
@@ -6,7 +6,8 @@ const user = new Schema({
     _id: ObjectId,
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    nickname: { type: String, required: true }
+    nickname: { type: String, required: true },
+    activated: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('User', user);
