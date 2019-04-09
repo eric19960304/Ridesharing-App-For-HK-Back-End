888c8023db3adb060b7401f37242ae75201a2137
Unknown <elven118@gmail.com>
2019-04-10 00:29:03 +0800

    Fix for constrain of rvGraph

--- matchingEngine/RVGraph.py
+++ matchingEngine/RVGraph.py
@@ -175,6 +175,7 @@ class RVGraph:
                         delayDistance = float('inf')
                         waitingDistance = float('inf')
                         minShareDistance = float('-inf')
+                        separatedDistance = float('inf')
 
                         if not driverPassagerList[0]["isOnCar"]:
                             #0: request startLocation, 1: request endLocation, 2: driver Location, 3: passager startLocation, 4: passager endLocation
@@ -183,15 +184,17 @@ class RVGraph:
                             distanceMatrix[2][3] + distanceMatrix[3][0] + distanceMatrix[0][4] + distanceMatrix[4][1],
                             distanceMatrix[2][3] + distanceMatrix[3][0] + distanceMatrix[0][1] + distanceMatrix[1][4]])
 
-                            delayDistance = minShareDistance - distanceMatrix[2][3] - distanceMatrix[3][4] 
+                            separatedDistance = distanceMatrix[2][3] + distanceMatrix[3][4] 
+                            delayDistance = minShareDistance - separatedDistance
                             waitingDistance = min(distanceMatrix[2][0], distanceMatrix[2][3])
                         else:
                             #0: request startLocation, 1: request endLocation, 2: driver Location, 3: passager endLocation
                             minShareDistance = min(distanceMatrix[2][0] + distanceMatrix[0][1] + distanceMatrix[1][3],
                             distanceMatrix[2][0] + distanceMatrix[0][3] + distanceMatrix[3][1])
 
-                            delayDistance = minShareDistance - distanceMatrix[2][3] - distanceMatrix[0][1]
-                            waitingDistance = min(distanceMatrix[2][0], distanceMatrix[2][3])
+                            separatedDistance = distanceMatrix[2][3] + distanceMatrix[0][1]
+                            delayDistance = minShareDistance - separatedDistance
+                            waitingDistance = distanceMatrix[2][0]
 
                         if self.satifiedAllConstraints(minShareDistance, separatedDistance, waitingDistance):
                             edgeList.append( (driver, request, delayDistance) )
