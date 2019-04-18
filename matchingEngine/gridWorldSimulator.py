d80dbc4b0b53819ac957243a6a2e2f9ba8f816d3
Eric Lau <ericlauchiho@gmail.com>
2019-04-18 10:38:15 +0800

    include unhandled waiting time

--- matchingEngine/gridWorldSimulator.py
+++ matchingEngine/gridWorldSimulator.py
@@ -183,14 +183,14 @@ class GridWorldSimulator:
             actualTravelTime = req['finishedDate'] - req['requestedDate']
             totalDelay += actualTravelTime - optimalTravelTime
         
-        # for req in self.requests:
-        #     totalWaitingTime += self.currentTime - req['requestedDate']
-        #     totalDelay += self.currentTime - req['requestedDate']
+        for req in self.requests:
+            totalWaitingTime += self.currentTime - req['requestedDate']
+            totalDelay += self.currentTime - req['requestedDate']
 
         # print('Total waiting time   = %d'%(totalWaitingTime))
         # print('Total delay          = %d'%(totalDelay))
-        reqLen = len(self.finishedRequests) 
-        # reqLen = len(self.finishedRequests) + len(self.requests)
+        # reqLen = len(self.finishedRequests) 
+        reqLen = len(self.finishedRequests) + len(self.requests)
         
         self.avgWaitingTime = totalWaitingTime/reqLen
         self.avgtotalDelay = totalDelay/reqLen
